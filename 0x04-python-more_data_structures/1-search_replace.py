e001f59978e2f216e9910d4e3d9e7e0c614d00d4
shalom90 <ibukoye92@gmail.com>
2023-01-11 17:43:45 -0800

    Task 1- search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 
 def search_replace(my_list, search, replace):
 	    new_list = my_list[:]
-	        for i in new_list:
-	            if i == search:
-	                new_list[new_list.index(i)] = replace
-	        return new_list
+	        for i in range(len(new_list)):
+			        if new_list[i] == search:
+				            new_list[i] = replace
+				           return new_list
